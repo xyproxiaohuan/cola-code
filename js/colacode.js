a62d0963103a5ed90558291eb4cc5f215690bc45
Yuxiaohuan <onlysome@XiaohuanYus-MacBook-Pro.local>
2016-04-19 12:15:20 -0400

    changed

--- js/colacode.js
+++ js/colacode.js
@@ -1,12 +1,19 @@
+(function() {
+	console.log('asdfasdfasdf');
+	var selectedSodaPrice = 0;
+  $('#Sodas li').on('click',function(){
+	  selectedSodaPrice = $(this).data('price');
+    $('#cost').text(selectedSodaPrice);
+  });
+  $('#get-soda').on('click',function(){
+	var amountPaid = $('#amount-paid').val();
+  var change = (amountPaid - selectedSodaPrice);
 
-(function(){
-  var selectedSodaPrice = 0;
-$('#Soda li').on('click',function(){
-  selectedSodaPrice = $(this).data('price');
-  $('#task').text(selectedSodaPrice);
-});
-$('#get-soda').on('click',function(){
- var amountPaid = $('#amount-paid').val();
- var charge = (amountPaid - selectedSodaPrice);
+  if(change < 0) {
+  	$('#output').text('NOT ENOUGH MONEY');
+  } else {
+  	$('#output').text("Your Change is $" + change);
+  }
 
 });
+})();
